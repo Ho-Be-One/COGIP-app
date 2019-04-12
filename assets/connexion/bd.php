41c3a24d2e162cbc867470433f5d44fcb0db0a2b
Jean-marc Broutin <jmb@MacBook-Pro-de-Jean-marc.local>
2019-04-12 14:55:26 +0200

    add screenshot

--- assets/connexion/bd.php
+++ assets/connexion/bd.php
@@ -2,7 +2,7 @@
 try
 	{
 		// On se connecte à MySQL
-		$bdd = new PDO('mysql:host=localhost;dbname=cogip;charset=utf8', 'root', 'root');
+		$bdd = new PDO('mysql:host=localhost;dbname=id9273965_cogip;charset=utf8', 'id9273965_becode', 'becode2019');
 		$bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 		$bdd->exec("SET NAMES 'UTF8'");		
 		}
